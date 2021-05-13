83e3752f315af55447e1ecbbfb4b3675353ac009
Kurtis Eberhardt <77414699+KurtisEberhardt@users.noreply.github.com>
2021-05-13 17:18:11 -0600

    working on color classes, almost done

--- app.js
+++ app.js
@@ -89,8 +89,34 @@ function updateActive(item) {
 
 function updateRupees() {
     document.getElementById('rupeeCount').innerText = totalRupees.toString()
-}
+    if(totalRupees>=1200){
+        document.getElementById('rupee').src="https://i.imgur.com/ogBlCtV.png"
+        document.getElementById('header').classList.replace('bg-orange', 'bg-silver')
+        document.getElementById('body').classList.replace('body-orange', 'body-silver')
+    }else if(totalRupees>=1000){
+        document.getElementById('rupee').src="https://i.imgur.com/O87Y42x.png"
+        document.getElementById('header').classList.replace('bg-purple', 'bg-orange')
+        document.getElementById('body').classList.replace('body-purple', 'body-orange')
+    }else if(totalRupees>=800){
+        document.getElementById('rupee').src="https://i.imgur.com/bX0RHhl.png"
+        document.getElementById('header').classList.replace('bg-red', 'bg-purple')
+        document.getElementById('body').classList.replace('body-red', 'body-purple')
+    }else if(totalRupees>=600){
+        document.getElementById('rupee').src="https://i.imgur.com/Adq8DdU.png"
+        document.getElementById('header').classList.replace('bg-yellow', 'bg-red')
+        document.getElementById('body').classList.replace('body-yellow', 'body-red')
 
+    }else if(totalRupees>=400){
+        document.getElementById('rupee').src="https://i.imgur.com/JAyFaY7.png"
+        document.getElementById('header').classList.replace('bg-blue', 'bg-yellow')
+        document.getElementById('body').classList.replace('body-blue', 'body-yellow')
+
+    }else if(totalRupees>=200){
+        document.getElementById('rupee').src="https://i.imgur.com/ZYd5Rbl.png"
+        document.getElementById('header').classList.replace('bg-success', 'bg-blue')
+        document.getElementById('body').classList.replace('bg-color', 'body-blue')
+    }
+}
 function collectPassives() {
         for (let key in passiveUpgrades) {
             let item = passiveUpgrades[key]
@@ -101,7 +127,7 @@ function collectPassives() {
 
     //TODO research update function with for loop
 function startInterval(){
-    collectionInterval = setInterval(collectPassives, 3000)
+    let collectionInterval = setInterval(collectPassives, 3000)
 }
 startInterval()
 
